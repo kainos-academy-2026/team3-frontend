21e88548115fb7aca3bb39fd9968ea4792c309b7
yasrig <yasmine.rigby@kainos.com>
2026-07-06 16:50:17 +0100

    US038 - Linting check

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,11 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/health', (req: express.Request, res: express.Response) => {
-  res.status(200).json({
-    status: "UP",
-    time: new Date().toISOString(),
-  });
+app.get("/health", (_req: express.Request, res: express.Response) => {
+	res.status(200).json({
+		status: "UP",
+		time: new Date().toISOString(),
+	});
 });
 
 export default app;
