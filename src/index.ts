ced1cea16b9caba620c6d04ae6701b31fe5dd3d9
John McAllister <johnmcallister11@hotmail.com>
2026-07-06 15:13:35 +0100

    Create frontend project (#2)
    * Created Front-End Project
    
    * adding pull request template
    
    * updating package.json
    
    * Fixes post Review
    
    * Fixes post review v2
    
    * Fixes post review v3

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+import express from "express";
+ 
+dotenv.config();
+ 
+const app = express();
+const PORT = Number(process.env.PORT) || 3000;
+ 
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/health', (req: express.Request, res: express.Response) => {
+  res.status(200).json({
+    status: "UP",
+    time: new Date().toISOString(),
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
