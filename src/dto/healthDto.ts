981c9ae1181187629ad1e71aee7a043ec8114fae
johnmcallister11 <johnmcallister11@hotmail.com>
2026-07-06 15:07:11 +0100

    Fixes post review v3

--- src/dto/healthDto.ts
+++
@@ -1,4 +0,0 @@
-export interface HealthCheckResponse {
-    status: string;
-    time: string;
-}
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,5 @@
 import dotenv from "dotenv";
 import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
- 
-import healthRouter from "./routes/healthRouter.js";
- 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
  
 dotenv.config();
  
@@ -14,10 +7,14 @@ const app = express();
 const PORT = Number(process.env.PORT) || 3000;
  
 app.use(express.urlencoded({ extended: true }));
- 
+
+app.get('/health', (req: express.Request, res: express.Response) => {
+  res.status(200).json({
+    status: "UP",
+    time: new Date().toISOString(),
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
- 
- 
-app.use(healthRouter);
